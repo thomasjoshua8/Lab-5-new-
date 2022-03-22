6a25cdf9689b87633f98ae2f9868164384459bed
Joshua Thomas <89352954+thomasjoshua8@users.noreply.github.com>
2022-03-22 00:48:57 +0000

    This is for Lab 6

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+//For new date
 function lastedit(){
   var lastestDate = new Date(document.lastModified);
   document.getElementById("time").innerHTML = "Last modified on: " + lastestDate;
 }
+//method for changing color to red
+function redColor(){
+  document.getElementById("button").style.backgroundColor = "red";
+}
+//method for changing color to blue
+function blueColor(){
+  document.getElementById("button").style.backgroundColor = "blue";
+}
+//method for changing color to yellow
+function yellowColor(){
+  document.getElementById("button").style.backgroundColor = "yellow";
+}
+
+function text(){
+  console.log("it is working");
+}
+
+lastedit();
